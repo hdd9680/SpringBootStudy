410be8b62d23ba0501d2dda77868600d82a903ce
DESKTOP-CABA51R\hdd96 <hdd9680@gmail.com>
2023-01-22 21:27:54 +0900

    logging 테스트

--- src/main/java/pri/hsy/springBootStudy/member/web/MemberController.java
+++ src/main/java/pri/hsy/springBootStudy/member/web/MemberController.java
@@ -2,16 +2,28 @@ package pri.hsy.springBootStudy.member.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/member")
 public class MemberController {
 	
 	@RequestMapping("/page/join")
-	public String movePageLogin() {
+	public String movePageLogin(@RequestParam(required = false, defaultValue = "0") int value) {
+		
+		if(value == 0) {
+			log.debug("debug log");
+		} else if(value == 1) {
+			log.info("info log");
+		} else if(value == 2) {
+			log.error("error log");
+		}
+		
 		return "member/join";
 	}
 	
